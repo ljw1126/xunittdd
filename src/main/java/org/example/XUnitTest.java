0d3d8fb4972a0c76e937b26c37e998c02a4270de
이진우 <leejinwoo1126@gmail.com>
2025-02-16 22:58:17 +0900

    refactor: TestCaseTest.suite() 정적 팩토리 메서드 추가

--- src/main/java/org/example/XUnitTest.java
+++ src/main/java/org/example/XUnitTest.java
@@ -2,12 +2,7 @@ package org.example;
 
 public class XUnitTest {
     public static void main(String[] args) {
-        TestSuite suite = new TestSuite();
-        suite.add(new TestCaseTest("testTemplateMethod"));
-        suite.add(new TestCaseTest("testResult"));
-        suite.add(new TestCaseTest("testFailedResultFormatting"));
-        suite.add(new TestCaseTest("testFailedResult"));
-        suite.add(new TestCaseTest("testSuite"));
+        TestSuite suite = TestCaseTest.suite();
 
         TestResult result = new TestResult();
         suite.run(result);
