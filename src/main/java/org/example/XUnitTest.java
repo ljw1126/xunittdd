cd58b2482b9922e75fb64c65d609d4229fbc69b9
이진우 <leejinwoo1126@gmail.com>
2025-02-16 22:13:34 +0900

    refactor: TestCaseTest 생성, method move

--- src/main/java/org/example/XUnitTest.java
+++ src/main/java/org/example/XUnitTest.java
@@ -2,9 +2,6 @@ package org.example;
 
 public class XUnitTest {
     public static void main(String[] args) {
-        WasRun wasRun = new WasRun("testMethod");
-        System.out.println(wasRun.wasRun); // false
-        wasRun.run();
-        System.out.println(wasRun.wasRun); // true
+       new TestCaseTest("testRunning").run();
     }
 }
