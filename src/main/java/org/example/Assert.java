4cf40c9890bc4876459cdb8af530770c5b15eb3c
이진우 <leejinwoo1126@gmail.com>
2025-02-16 22:16:24 +0900

    feat: Assert 클래스 생성, assertEquals() 정적 메서드 추가

---
+++ src/main/java/org/example/Assert.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Assert {
+    public static void assertEquals(Object expected, Object actual) {
+        if(!expected.equals(actual)) {
+            throw new AssertionError("expected <" + expected + "> but was <" + actual + ">");
+        }
+    }
+}
