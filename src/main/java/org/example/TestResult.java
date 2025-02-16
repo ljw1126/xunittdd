7cf496db16d963578bde96c740e1647f9b21a784
이진우 <leejinwoo1126@gmail.com>
2025-02-16 22:38:47 +0900

    test: testFailedResultFormatting() 추가

--- src/main/java/org/example/TestResult.java
+++ src/main/java/org/example/TestResult.java
@@ -2,12 +2,17 @@ package org.example;
 
 public class TestResult {
     int runCount = 0;
+    int errorCount = 0;
 
     public void testStarted() {
         runCount += 1;
     }
 
+    public void testFailed() {
+        errorCount += 1;
+    }
+
     public String getSummary() {
-        return runCount + " run, 0 failed";
+        return runCount + " run, " + errorCount + " failed";
     }
 }
